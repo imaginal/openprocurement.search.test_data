57dd91ee6085554a8e269fa7c45f0b9f98a7b001
Volodymyr Flonts <flyonts@gmail.com>
2017-08-18 22:59:07 +0300

    update auction ids, add auctions load script

---
+++ test_load_auctions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+CONCURENCY=10
+REQUESTS=100
+NOT_FOUND=40
+
+/opt/search-tenders/bin/test_load \
+	-c $CONCURENCY -n $REQUESTS -z $NOT_FOUND \
+	--aid auction3_id.json \
+    --cav cav.json \
+    --cpv cpv.json \
+    --cpvs cpvs.json \
+    --date date.json \
+    --edrpou auction_edrpou.json \
+    --region region.json \
+    --item_region region.json \
+    --item_square square.json \
+    --value value.json \
+	--status status.json \
+	--query auction_query.json \
+	--sort sort.json \
+	--log test_load_auctions.log \
+    http://127.0.0.1:8484/auctions?index=3
+
